654c775a8ad778f6af55f8272542dfc85ec05f4c
SaridakisStamatisChristos <34583142+SaridakisStamatisChristos@users.noreply.github.com>
2026-09-02 18:03:48 +0300

    Fix C linkage for CPU detection API

--- include/thermal/simd/thermal_cpu.h
+++ include/thermal/simd/thermal_cpu.h
@@ -6,6 +6,10 @@
 #include <thermal/simd/simd_width.h>
 #include <thermal/simd/thermal_config.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern uint8_t g_tsd_avx_available;
 
 simd_width_t tsd_detect_max_simd(const tsd_runtime_config *cfg);
@@ -15,6 +19,10 @@ int tsd_cpu_has_sse41(void);
 void tsd_cpu_set_detect_override(simd_width_t (*fn)(void));
 void tsd_cpu_clear_detect_override(void);
 simd_width_t tsd_cpu_detect_ignoring_override(const tsd_runtime_config *cfg);
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif /* TSD_THERMAL_CPU_H */
